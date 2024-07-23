eea18639a354b492c070a0890ce59035ee2f3570
Rajesh Kumar <rajakumarlohano@gmail.com>
2024-07-23 12:48:46 +0800

    Add files via upload

--- operate/main.py
+++ operate/main.py
@@ -17,6 +17,30 @@ def main_entry():
         required=False,
         default="gpt-4-with-ocr",
     )
+    parser.add_argument(
+        "--download-video",
+        help="URL of the YouTube video to download",
+        type=str,
+    )
+    parser.add_argument(
+        "--copy-text",
+        nargs=2,
+        metavar=('SOURCE_FILE', 'DESTINATION_FILE'),
+        help="Copy text from source to destination file",
+    )
+    parser.add_argument(
+        "--fill-form",
+        help="URL of the form to fill and upload documents",
+        type=str,
+    )
+
+    
+    parser.add_argument(
+    "--scrape-url",
+    help="URL to scrape data from",
+    type=str,
+    required=False
+)
 
     # Add a voice flag
     parser.add_argument(
@@ -42,6 +66,15 @@ def main_entry():
 
     try:
         args = parser.parse_args()
+        if args.scrape_url:
+        scrape_process(args.scrape_url)
+        elif args.download_video:
+        download_youtube_video(args.download_video, '/path/to/download')
+        elif args.copy_text:
+        copy_text(*args.copy_text)
+        elif args.fill_form:
+        fill_form_and_upload_document(args.fill_form)
+        else:
         main(
             args.model,
             terminal_prompt=args.prompt,
